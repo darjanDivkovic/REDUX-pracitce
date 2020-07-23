98f5a4b9a92163698110259ad11b24bd421524cc
Darjan Divkovic <daarjan.tz@gmail.com>
2020-07-23 17:33:15 +0200

    Learning about redux store, actions, reducers, pure functions, dispatchers, listeners...

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-function createStore(){
-     // The store should have 4 parts
-     // 1. The state
-     // 2. Get the state
-     let state = 'a'
+// Library Code
+function createStore (reducer) {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+  
+    let state
+    let listeners = []
+  
+    const getState = () => state
+  
+    const subscribe = (listener) => {
+      listeners.push(listener)
+      return () => {
+        listeners = listeners.filter((l) => l !== listener)
+      }
+    }
+  
+    const dispatch = (action) => {
+      state = reducer(state, action)
+      listeners.forEach((listener) => listener())
+    }
+  
+    return {
+      getState,
+      subscribe,
+      dispatch,
+    }
+  }
+  
+  // App Code
+  function todos (state = [], action) {
+    if (action.type === 'ADD_TODO') {
+      return state.concat([action.todo])
+    }
+  
+    return state
+  }
 
-     const getState = () => state
-     // 3. Listen to changes on the state
-     // 4. Update the state
-
-     return {
-         getState
-     }
-}
-
-let store = createStore();
-let state = store.getState();
-
-// Got the state
-console.log(state);
+const store = createStore(todos)
+store.subscribe(() => {
+    console.log('The state has changed: new state', store.getState());
+})
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learning Redux',
+        complete: false,
+    }
+})
